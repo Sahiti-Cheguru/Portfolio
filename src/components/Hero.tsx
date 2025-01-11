8972e820bc63707556b51a08106fee67bd278182
Sri Ganesh Shiramshetty <115027993+GaneshVarma1@users.noreply.github.com>
2025-01-10 21:07:06 -0500

    Added New Design component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,6 +5,37 @@ import { FloatingEmoji } from "./FloatingEmoji";
 import { SocialLink } from "./SocialLink";
 import HeroImage from "../assets/Images/Hero.png";
 import { WavingHand } from "./WavingHand";
+import AvatarCircles from "@/components/ui/avatar-circles";
+import { ArrowRight } from "lucide-react";
+import { cn } from "@/lib/utils";
+import AnimatedShinyText from "@/components/ui/animated-shiny-text";
+
+const avatars = [
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/16860528",
+    profileUrl: "/",
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/20110627",
+    profileUrl: "/",
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/106103625",
+    profileUrl: "/",
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/59228569",
+    profileUrl: "/",
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/59442788",
+    profileUrl: "/",
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/89768406",
+    profileUrl: "/",
+  },
+];
 
 const emojis = [
   { emoji: "💻", delay: 0, duration: 4, position: { x: 75, y: 20 } },
@@ -65,6 +96,17 @@ export function Hero() {
                 className="w-auto h-5"
               />
             </h5>
+            <div className="flex items-center space-x-2">
+              <span className="text-xl font-bold text-gray-700 dark:text-gray-300">
+                Used by
+              </span>
+              <div>
+                <AvatarCircles numPeople={99} avatarUrls={avatars} />
+              </div>
+              <span className="text-xl font-bold text-gray-700 dark:text-gray-300">
+                Developers.
+              </span>
+            </div>
             <div className="flex gap-6 pt-4">
               <SocialLink
                 href="https://github.com/GaneshVarma1"
@@ -104,9 +146,31 @@ export function Hero() {
             initial={{ x: 50, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{ duration: 0.8, delay: 0.4 }}
-            className="w-full md:w-1/2 flex justify-center items-center relative"
+            className="w-full md:w-1/2 flex flex-col items-center relative"
           >
-            <div className="relative">
+            {/* Magic UI Section */}
+            <div className="z-10 flex items-center justify-center mb-4">
+              <div
+                className={cn(
+                  "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800"
+                )}
+              >
+                <a
+                  href="https://reactwind.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-block"
+                >
+                  <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
+                    <span>✨ Checkout ReactWind</span>
+                    <ArrowRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+                  </AnimatedShinyText>
+                </a>
+              </div>
+            </div>
+
+            {/* Hero Image */}
+            <div className="relative mt-0">
               <div className="absolute inset-0 bg-gradient-to-r from-blue-500 to-purple-500 rounded-[15px] blur-2xl opacity-20 animate-pulse" />
               <motion.img
                 whileHover={{ scale: 1.02 }}
