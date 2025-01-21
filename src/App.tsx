807cb6dd07f26f8f743554550e45862e3d667ec1
Sri Ganesh Shiramshetty <115027993+GaneshVarma1@users.noreply.github.com>
2025-01-20 19:31:33 -0500

    Added Cursor Effect

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,63 @@ import { Footer } from "./components/footer/Footer";
 import { Preview } from "./components/Preview";
 import { ScrollToTop } from "./components/ScrollToTop";
 import { ThemeProvider } from "./components/ThemeProvider";
+import AnimatedCursor from "react-animated-cursor";
+import { useTheme } from "./components/ThemeProvider";
+
+function AppContent() {
+  const { theme } = useTheme();
+
+  return (
+    <div className="min-h-screen bg-white dark:bg-gray-900">
+      {/* Only show cursor on non-touch devices */}
+      {typeof window !== "undefined" && !("ontouchstart" in window) && (
+        <AnimatedCursor
+          innerSize={8}
+          outerSize={35}
+          color={theme === "dark" ? "255, 255, 255" : "0, 0, 0"}
+          outerAlpha={0.3}
+          innerScale={0.7}
+          outerScale={2.5}
+          clickables={[
+            "a",
+            'input[type="text"]',
+            'input[type="email"]',
+            'input[type="number"]',
+            'input[type="submit"]',
+            'input[type="image"]',
+            "label[for]",
+            "select",
+            "textarea",
+            "button",
+            ".link",
+          ]}
+          outerStyle={{
+            border: "2px solid currentColor",
+            mixBlendMode: "exclusion",
+          }}
+          innerStyle={{
+            backgroundColor: "currentColor",
+            mixBlendMode: "exclusion",
+          }}
+        />
+      )}
+      <CursorEffect />
+      <Navigation />
+      <Hero />
+      <About />
+      <ExperienceSection />
+      <Projects />
+      <Preview />
+      <Footer />
+      <ScrollToTop />
+    </div>
+  );
+}
 
 function App() {
   return (
     <ThemeProvider>
-      <div className="min-h-screen bg-white dark:bg-gray-900">
-        <CursorEffect />
-        <Navigation />
-        <Hero />
-        <About />
-        <ExperienceSection />
-        <Projects />
-        <Preview />
-        <Footer />
-        <ScrollToTop />
-      </div>
+      <AppContent />
     </ThemeProvider>
   );
 }
