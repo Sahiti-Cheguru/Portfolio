a29c7c557c882eede89ff402490dac07b5a71736
Sri Ganesh Shiramshetty <115027993+GaneshVarma1@users.noreply.github.com>
2025-01-31 18:40:32 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,20 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "framer-motion"],
+          utils: ["@/lib/utils"],
+          matter: ["matter-js", "poly-decomp"],
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
